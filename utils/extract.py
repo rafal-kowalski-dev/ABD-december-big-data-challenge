45e1f5890317cbc78221f35f936c33c8301e42ce
Rafał Kowalski <rafal.kowalski@rkowalski.dev>
2024-12-08 23:36:29 +0100

    first step - geting data from API and save to file

---
+++ utils/extract.py
@@ -0,0 +1,94 @@
+import json
+import logging
+from datetime import date, timedelta
+
+import requests
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    filename="log.txt",
+    filemode="a",
+)
+
+
+# Wroclaw
+# coordinates = (51.10536196888788, 17.01631922567193)
+
+# Szczerbice
+coordinates = (50.092402238216856, 18.459952564477)
+latitude = coordinates[0]
+longitude = coordinates[0]
+
+
+past_days = None
+forecast = 1
+
+url = f"https://archive-api.open-meteo.com/v1/era5" if past_days else f'https://api.open-meteo.com/v1/forecast'
+url += f'?latitude={latitude}&longitude={longitude}'
+
+cols = [
+    "temperature_2m",
+    "relative_humidity_2m",
+    "dew_point_2m",
+    "apparent_temperature",
+    "precipitation_probability",
+    "precipitation",
+    "rain",
+    "showers",
+    "snowfall",
+    "snow_depth",
+    "weather_code",
+    "pressure_msl",
+    "surface_pressure",
+    "cloud_cover",
+    "cloud_cover_low",
+    "cloud_cover_mid",
+    "cloud_cover_high",
+    "visibility",
+    "evapotranspiration",
+    "et0_fao_evapotranspiration",
+    "vapour_pressure_deficit",
+    "wind_speed_10m",
+    "wind_speed_80m",
+    "wind_speed_120m",
+    "wind_speed_180m",
+    "wind_direction_10m",
+    "wind_direction_80m",
+    "wind_direction_120m",
+    "wind_direction_180m",
+    "wind_gusts_10m",
+    "temperature_80m",
+    "temperature_120m",
+    "temperature_180m",
+    "soil_temperature_0cm",
+    "soil_temperature_6cm",
+    "soil_temperature_18cm",
+    "soil_temperature_54cm",
+    "soil_moisture_0_to_1cm",
+    "soil_moisture_1_to_3cm",
+    "soil_moisture_3_to_9cm",
+    "soil_moisture_9_to_27cm",
+    "soil_moisture_27_to_81cm",
+]
+
+today = date.today()
+
+if past_days:
+    url += f"&start_date={today - timedelta(days=past_days)}&end_date={today}"
+
+if forecast:
+    url += f'&forecast_days={forecast}'
+
+if len(cols):
+    url += f"&hourly={','.join(cols)}"
+
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    with open("weather.json", mode="w", encoding="utf8") as out_file:
+        out_file.write(response.text)
+else:
+    logging.error(f"{response.status_code} -> {json.loads(response.text)['reason']}")
