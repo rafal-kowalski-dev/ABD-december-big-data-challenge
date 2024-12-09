d0ff68fa64210c277f4847522494e069531868ad
Rafał Kowalski <rafal.kowalski@rkowalski.dev>
2024-12-10 00:29:48 +0100

    Move folder creation utility to WeatherAppConfig

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 """Configuration settings for the weather data application."""
 
 import json
+import os
 from pathlib import Path
 from typing import List
 
@@ -16,7 +17,7 @@ class WeatherAppConfig(BaseModel):
     def _update(self, conf: dict):
         self.forecast = conf["forecast"]
         self.cities = conf["cities"]
-        
+
     def import_settings_from_json(self, path: Path | str, encoding="utf8"):
         try:
             with open(path, encoding=encoding) as f:
@@ -27,6 +28,14 @@ class WeatherAppConfig(BaseModel):
         except Exception as e:
             logger.error(f"{self.__class__.__name__} -> {e}")
 
+    @staticmethod
+    def check_nessesary_folders():
+        if not os.path.exists("results/"):
+            os.mkdir("results/")
+        if not os.path.exists("logs/"):
+            os.mkdir("logs/")
+
 
 weather_app_config = WeatherAppConfig()
+weather_app_config.check_nessesary_folders()
 weather_app_config.import_settings_from_json("config.json")
