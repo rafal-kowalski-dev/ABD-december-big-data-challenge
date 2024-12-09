4a0d08300a5bfb51cbb67b08a2ca87527a078218
Rafał Kowalski <rafal.kowalski@rkowalski.dev>
2024-12-10 00:13:23 +0100

    Update logger configuration to save logs in a dated directory structure

--- utils/logger.py
+++ utils/logger.py
@@ -1,13 +1,14 @@
 """Logging configuration module for the application."""
 
 import logging
+from datetime import date
 from logging import Logger
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
-    filename="log.txt",
+    filename=f"logs/{date.today()}.log",
     filemode="a",
     encoding="utf8",
 )
