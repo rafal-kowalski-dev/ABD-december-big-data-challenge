5079e0c0f9ebdabfa7630201f0ed3b52680c3e1a
Rafal Kowalski <rafal.kowalski@rkowalski.dev>
2024-12-28 21:51:36 +0000

    Refactor convert_json_to_parquet function to simplify Spark session initialization and remove unused date extraction logic.

--- transform_data/convert_json_to_parquet.py
+++ transform_data/convert_json_to_parquet.py
@@ -25,21 +25,13 @@ def convert_json_to_parquet(path: Path | str, spark_session: SparkSession = None
         None: The function saves the weather data to Parquet and logs the result.
     """
     if spark_session is None:
-        spark_session = (
-            SparkSession.builder.appName("YourAppName")
-            .config("spark.security.manager.enabled", "false")
-            .getOrCreate()
-        )
+        spark_session = SparkSession.builder.getOrCreate()
 
     try:
         filename = path.name
     except AttributeError:
         filename = path.split("/")[-1].split(".json")[0]
-    y, m, d = [int(x) for x in filename.split("-")]
 
     json_data = spark_session.read.option("multiline", "true").json(path)
-    json_data = json_data.withColumn("measure_date", lit(date(y, m, d)))
 
     json_data.write.parquet(f"results/parquets/{filename}.parquet", mode="overwrite")
-
-    spark_session.read.parquet(f"results/parquets/{filename}.parquet")
