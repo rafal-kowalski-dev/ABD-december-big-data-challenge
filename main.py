8d27e78236cb89e8c9333a018d42e0cecfe6d7b3
Rafal Kowalski <rafal.kowalski@rkowalski.dev>
2024-12-29 18:26:48 +0000

    Refactor main.py to implement command-line interface for weather data operations

--- main.py
+++ main.py
@@ -2,14 +2,58 @@
 
 import json
 from datetime import date
-from click import command, option
+from os import listdir
+
+import click
 
 from config import weather_app_config
 from get_data import get_coordinates, get_weather_data
-from transform_data import convert_json_to_parquet
+from transform_data import Measurement, MesurementType, convert_json_to_parquet
 from utils.logger import logger
 
-if __name__ == "__main__":
+
+@click.command()
+@click.option(
+    "-w", "--get-weather-data", is_flag=True, help="Get weather data", default=False
+)
+@click.option(
+    "-p",
+    "--convert-json-to-parquet",
+    is_flag=True,
+    help="Convert json to parquet",
+    default=False,
+)
+@click.option(
+    "-c", "--transform-data", is_flag=True, help="Transform data", default=False
+)
+@click.option("-o", "--output-path", type=str, help="Output path for clean data")
+@click.option(
+    "-i",
+    "--input-path",
+    type=str,
+    help="Input path for clean data [defailt=results/parquets/]",
+)
+def main(**kwargs):
+    """Main entry point for the weather data fetching application."""
+    if kwargs["get_weather_data"]:
+        get_weather_data_command()
+    if kwargs["convert_json_to_parquet"]:
+        convert_json_to_parquet_command()
+    if kwargs["transform_data"]:
+        transform_data_command(kwargs["output_path"], kwargs["input_path"])
+    if not any(
+        [
+            kwargs["get_weather_data"],
+            kwargs["convert_json_to_parquet"],
+            kwargs["transform_data"],
+        ]
+    ):
+        get_weather_data_command()
+        convert_json_to_parquet_command()
+        transform_data_command(kwargs["output_path"], kwargs["input_path"])
+
+
+def get_weather_data_command():
     path = f"results/{date.today()}.json"
     results = []
 
@@ -23,7 +67,22 @@ if __name__ == "__main__":
         json.dump(results, f)
         logger.info("main -> write data to file")
 
+
+def convert_json_to_parquet_command():
+    path = f"results/{date.today()}.json"
     convert_json_to_parquet(path)
     logger.info("main -> convert json to parquet")
 
-    
+
+def transform_data_command(output_path: str = None, input_path: str = None):
+    if not output_path:
+        output_path = f"results/clean_parquets/"
+    if not input_path:
+        input_path = f"results/parquets/"
+    meas = Measurement(input_path)
+    meas.save(f"{output_path}/{t.value}")
+    logger.info(f"main -> transform data -> {t.value}")
+
+
+if __name__ == "__main__":
+    main()
